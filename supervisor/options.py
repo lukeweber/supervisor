922637edf98ee80e1d5b792118c5015935288a05
Luke Weber <luke.weber@gmail.com>
2014-07-15 18:28:55 -0700

    - Patch for status codes on all errors in supervisorctl using https://github.com/Supervisor/supervisor/pull/292 as a starting point.

--- supervisor/options.py
+++ supervisor/options.py
@@ -1559,6 +1559,7 @@ class ClientOptions(Options):
     positional_args_allowed = 1
 
     interactive = None
+    supress_exit = None
     prompt = None
     serverurl = None
     username = None
@@ -1570,6 +1571,7 @@ class ClientOptions(Options):
         self.configroot = Dummy()
         self.configroot.supervisorctl = Dummy()
         self.configroot.supervisorctl.interactive = None
+        self.configroot.supervisorctl.supress_exit = None
         self.configroot.supervisorctl.prompt = 'supervisor'
         self.configroot.supervisorctl.serverurl = None
         self.configroot.supervisorctl.username = None
@@ -1584,6 +1586,8 @@ class ClientOptions(Options):
 
         self.add("interactive", "supervisorctl.interactive", "i",
                  "interactive", flag=1, default=0)
+        self.add("supress_exit", "supervisorctl.supress_exit", "e",
+                 "supress_exit", flag=1, default=0)
         self.add("prompt", "supervisorctl.prompt", default="supervisor")
         self.add("serverurl", "supervisorctl.serverurl", "s:", "serverurl=",
                  url, default="http://localhost:9001")
@@ -1595,6 +1599,7 @@ class ClientOptions(Options):
         Options.realize(self, *arg, **kw)
         if not self.args:
             self.interactive = 1
+            self.supress_exit = 1
 
     def read_config(self, fp):
         section = self.configroot.supervisorctl
