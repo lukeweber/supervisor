e60422f602cdf2721bdb549471807e7349fd2d3a
Luke Weber <luke.weber@gmail.com>
2015-07-16 10:33:18 -0700

    change supress_exit to exit_on_error and cleanup

--- supervisor/options.py
+++ supervisor/options.py
@@ -1559,7 +1559,7 @@ class ClientOptions(Options):
     positional_args_allowed = 1
 
     interactive = None
-    supress_exit = None
+    exit_on_error = None
     prompt = None
     serverurl = None
     username = None
@@ -1571,7 +1571,7 @@ class ClientOptions(Options):
         self.configroot = Dummy()
         self.configroot.supervisorctl = Dummy()
         self.configroot.supervisorctl.interactive = None
-        self.configroot.supervisorctl.supress_exit = None
+        self.configroot.supervisorctl.exit_on_error = None
         self.configroot.supervisorctl.prompt = 'supervisor'
         self.configroot.supervisorctl.serverurl = None
         self.configroot.supervisorctl.username = None
@@ -1586,8 +1586,8 @@ class ClientOptions(Options):
 
         self.add("interactive", "supervisorctl.interactive", "i",
                  "interactive", flag=1, default=0)
-        self.add("supress_exit", "supervisorctl.supress_exit", "e",
-                 "supress_exit", flag=1, default=0)
+        self.add("exit_on_error", "supervisorctl.exit_on_error", "x",
+                 "exit_on_error", flag=1, default=0)
         self.add("prompt", "supervisorctl.prompt", default="supervisor")
         self.add("serverurl", "supervisorctl.serverurl", "s:", "serverurl=",
                  url, default="http://localhost:9001")
@@ -1599,7 +1599,7 @@ class ClientOptions(Options):
         Options.realize(self, *arg, **kw)
         if not self.args:
             self.interactive = 1
-            self.supress_exit = 1
+            self.exit_on_error = not self.interactive
 
     def read_config(self, fp):
         section = self.configroot.supervisorctl
